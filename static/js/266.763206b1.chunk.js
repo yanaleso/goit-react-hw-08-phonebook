5a22e4fe0dbe7c99be4376fc4315073d6ea2166d
yanaleso <91130832+yanaleso@users.noreply.github.com>
2022-09-06 20:48:06 +0000

    Deploying to gh-pages from @ yanaleso/goit-react-hw-08-phonebook@f778cf52a963d46d4d59ba552b389126fdd23f27 🚀

---
+++ static/js/266.763206b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{9055:function(e,n,t){t.d(n,{x:function(){return c}});var i=t(6031),r=t(407),c=(0,i.ZP)("div")(r.$_,r.Dh,r.bK,r.GQ,r.Oq,r.Cg,r.FK)},1266:function(e,n,t){t.r(n);var i=t(9055),r=t(3329);n.default=function(){return(0,r.jsx)(i.x,{as:"main",display:"flex",flexDirection:"column",alignItems:"center",children:(0,r.jsx)("h1",{children:"Let's start!"})})}}}]);
+//# sourceMappingURL=266.763206b1.chunk.js.map
